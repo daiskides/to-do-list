af04e277d2d0b4ddeb03a92d275ce8b57583c449
camaco81 <camaco81@gmail.com>
2024-07-08 11:47:21 -0400

    ajustes en js/auth-register.js funcion para validar que las contraseñas sean iguales

--- js/auth-register.js
+++ js/auth-register.js
@@ -1,6 +1,9 @@
 
 let x = document.getElementById("clave");
 let y = document.getElementById("confirmClave");
+const inputs = document.querySelectorAll("input");
+
+
 
 
 function showpassword() {
@@ -13,3 +16,37 @@ function showpassword() {
 	}
 }
 
+
+function validarClaves(){
+	if (x.value == y.value) {
+		console.log(" son iguales");
+	}
+	else{
+		console.log("no son iguales");
+	}
+	document.getElementById("form1").submit();
+}
+
+
+const validarPassword = () => {
+	const inputPassword1 = document.getElementById('clave');
+	const inputPassword2 = document.getElementById('confirmClave');
+	const textError= document.getElementById("textError");
+
+	if(inputPassword1.value !== inputPassword2.value){
+		textError.classList.remove("d-none");
+		textError.classList.add("d-block");
+		textError.classList.remove("text-white");
+		textError.classList.add("text-danger");
+		
+	} else {
+		textError.classList.remove("d-block");
+		textError.classList.add("d-none");
+
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarPassword);
+	input.addEventListener('blur', validarPassword);
+});
